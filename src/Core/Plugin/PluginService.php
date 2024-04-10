f84f0e3df108cb0589c46278abab77053a8abfa8
Miljan Puzović <mpuzovic@lexo.ch>
2024-04-10 13:36:12 +0200

    Update PluginService.php

--- src/Core/Plugin/PluginService.php
+++ src/Core/Plugin/PluginService.php
@@ -239,7 +239,7 @@ class PluginService extends Singleton
             ->setVersion(VERSION)
             ->setRemotePath(UPDATE_PATH)
             ->setCacheKey(CACHE_KEY)
-            ->setCacheExpiration(2 * HOUR_IN_SECONDS)
+            ->setCacheExpiration(12 * HOUR_IN_SECONDS)
             ->setCache(true);
     }
 
